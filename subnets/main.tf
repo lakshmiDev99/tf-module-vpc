42113b56064f91b9f3636eee28fa12b9e79f6c28
lakshmiDev99 <chakkalakshmi92@gmail.com>
2023-09-04 12:41:24 +0530

    subnets

--- subnets/main.tf
+++ subnets/main.tf
@@ -3,7 +3,15 @@ resource "aws_subnet" "main" {
   vpc_id     = var.vpc_id
   cidr_block = each.value["cidr"]
   availability_zone = each.value["az"]
+  tags {
+    Name = each.key
+  }
 }
-variable "subnets"{}
-variable "vpc_id"{}
 
+resource "aws_route_table" "example" {
+  for_each = var.subnets
+  vpc_id = var.vpc_id
+  tags = {
+    Name = each.key
+  }
+}
