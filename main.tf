1f3fb07c25d376d41449b49c2b687c43358a0836
lakshmiDev99 <chakkalakshmi92@gmail.com>
2023-09-06 17:52:14 +0530

    peering

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_vpc" "main" {
   cidr_block = var.cidr
- // tags       = merge(local.tags, { Name = "${var.env}-vpc" })
+ tags       = merge(local.tags, { Name = "${var.env}-vpc" })
 }
 
 module "subnets" {
@@ -8,13 +8,13 @@ module "subnets" {
   for_each = var.subnets
   subnets  = each.value
   vpc_id   = aws_vpc.main.id
-#  tags     = local.tags
-#  env      = var.env
+  tags     = local.tags
+  env      = var.env
 }
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
-  //tags   = merge(local.tags, { Name = "${var.env}-igw" })
+  tags   = merge(local.tags, { Name = "${var.env}-igw" })
 }
 
 resource "aws_route" "igw" {
@@ -47,7 +47,7 @@ resource "aws_vpc_peering_connection" "peering" {
   peer_vpc_id = aws_vpc.main.id
   vpc_id      = var.default_vpc_id
   auto_accept = true
-  //tags        = merge(local.tags, { Name = "${var.env}-peer" })
+  tags        = merge(local.tags, { Name = "${var.env}-peer" })
 }
 
 resource "aws_route" "peer" {
@@ -67,29 +67,4 @@ resource "aws_instance" "main" {
   ami = "ami-03265a0778a880afb"
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
   subnet_id = local.app_subnet_id[0]
-}
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    description      = "TLS from VPC"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = [0.0.0.0/0]
-     }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-#  tags = {
-#    Name = "allow_tls"
-#  }
 }
