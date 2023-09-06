2e2896ed040c32e9ea60d783cb8f3f53ba2924b2
lakshmiDev99 <chakkalakshmi92@gmail.com>
2023-09-06 14:03:31 +0530

    peering

--- main.tf
+++ main.tf
@@ -42,9 +42,6 @@ resource "aws_route" "ngw" {
   destination_cidr_block = "0.0.0.0/0"
   nat_gateway_id         = element(aws_nat_gateway.ngw.*.id, count.index)
 }
-output "subnet" {
-  value = module.subnets
-}
 
 resource "aws_vpc_peering_connection" "peering" {
   peer_vpc_id = aws_vpc.main.id
@@ -67,5 +64,32 @@ resource "aws_route" "default-vpc-peer-entry" {
 }
 resource "aws_instance" "main" {
   instance_type = "t3.micro"
-  image_id = "ami-03265a0778a880afb"
+  ami = "ami-03265a0778a880afb"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  subnet_id = local.app_subnet_id[0]
+}
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [0.0.0.0/0]
+     }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
 }
