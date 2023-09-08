8c304a6ebc2cae3f561fbf5002cc5d4997141783
lakshmiDev99 <chakkalakshmi92@gmail.com>
2023-09-08 15:50:10 +0530

    load balancer

--- main.tf
+++ main.tf
@@ -27,34 +27,33 @@ resource "aws_route" "igw" {
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.igw.id
 }
+#
+#output "subnet" {
+#  value = module.subnets
+#}
 
-output "subnet" {
-  value = module.subnets
+output "public_subnet_ids" {
+  value = local.public_subnet_ids
 }
 
 resource "aws_eip" "ngw" {
-  for_each               = lookup(lookup(module.subnets, "public", null), "subnet_ids", null)
-  #  count  = length(local.public_subnet_ids)
+  count  = length(local.public_subnet_ids)
   domain = "vpc"
 }
 #
 resource "aws_nat_gateway" "ngw" {
-  for_each               = lookup(lookup(module.subnets, "public", null), "subnet_ids", null)
-  allocation_id = lookup(lookup(aws_eip.ngw,each.key,null),"id",null)
-  subnet_id = each.value["id"]
-
-#  count         = length(local.public_subnet_ids)
-#  allocation_id = element(aws_eip.ngw.*.id, count.index)
-#  subnet_id     = element(local.public_subnet_ids, count.index)
+  count         = length(local.public_subnet_ids)
+  allocation_id = element(aws_eip.ngw.*.id, count.index)
+  subnet_id     = element(local.public_subnet_ids, count.index)
 #  tags          = merge(local.tags, { Name = "${var.env}-ngw" })
 }
-#
-#resource "aws_route" "ngw" {
-#  count                  = length(local.private_route_table_ids)
-#  route_table_id         = element(local.private_route_table_ids, count.index)
-#  destination_cidr_block = "0.0.0.0/0"
-#  nat_gateway_id         = element(aws_nat_gateway.ngw.*.id, count.index)
-#}
+
+resource "aws_route" "ngw" {
+  count                  = length(local.private_route_table_ids)
+  route_table_id         = element(local.private_route_table_ids, count.index)
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = element(aws_nat_gateway.ngw.*.id, count.index)
+}
 #
 #resource "aws_vpc_peering_connection" "peering" {
 #  peer_vpc_id = aws_vpc.main.id
