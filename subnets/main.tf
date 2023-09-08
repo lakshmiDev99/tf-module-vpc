df3b2f175b7d77ee23b51ffd390c7d23a8bcee96
lakshmiDev99 <chakkalakshmi92@gmail.com>
2023-09-08 16:39:24 +0530

    load balancer

--- subnets/main.tf
+++ subnets/main.tf
@@ -3,10 +3,7 @@ resource "aws_subnet" "main" {
   vpc_id     = var.vpc_id
   cidr_block = each.value["cidr"]
   availability_zone = each.value["az"]
-  tags = {
-    Name=each.key
-  }
-#  tags       = merge(var.tags, { Name = "${var.env}-${each.key}-subnet" })
+  tags       = merge(var.tags, { Name = "${var.env}-${each.key}-subnet" })
   }
 
 
@@ -14,10 +11,7 @@ resource "aws_subnet" "main" {
 resource "aws_route_table" "main" {
   for_each = var.subnets
   vpc_id = var.vpc_id
-  tags = {
-    Name=each.key
-  }
-#  tags       = merge(var.tags, { Name = "${var.env}-${each.key}-rt" })
+  tags       = merge(var.tags, { Name = "${var.env}-${each.key}-rt" })
 }
 #
 resource "aws_route_table_association" "a" {
--- vars.tf
+++ vars.tf
@@ -3,6 +3,6 @@ variable "subnets" {}
 variable "default_vpc_id" {}
 variable "default_vpc_cidr" {}
 variable "default_vpc_route_table_id" {}
-#variable "tags" {}
-#variable "env" {}
+variable "tags" {}
+variable "env" {}
 #variable "sg_ingress_cidr" {}
