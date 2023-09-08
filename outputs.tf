03cef1ac83b9981c78760f2d8640e1774d3ba7a8
lakshmiDev99 <chakkalakshmi92@gmail.com>
2023-09-08 16:14:43 +0530

    load balancer

---
+++ outputs.tf
@@ -0,0 +1,13 @@
+
+output "subnets" {
+  value = module.subnets
+}
+
+output "public_subnet_ids" {
+  value = local.public_subnet_ids
+}
+
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
--- subnets/outputs.tf
+++ subnets/outputs.tf
@@ -1,19 +1,7 @@
-#output "subnet_ids" {
-#  value = aws_subnet.main
-#}
-#output "route_table_ids" {
-#  value = aws_route_table.main
-#}
-
-#output "subnet" {
-#  value = module.subnets
-#}
-
-output "public_subnet_ids" {
-  value = local.public_subnet_ids
+output "subnet_ids" {
+  value = aws_subnet.main
+}
+output "route_table_ids" {
+  value = aws_route_table.main
 }
 
-
-#output "vpc_id" {
-#  value = aws_vpc.main.id
-#}
